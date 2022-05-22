9a06ff260bb7e57afb88435766f4cabcf740b39c
AshrafulIslam5 <ashraful.islam.mihad@gmail.com>
2022-05-22 17:53:39 +0600

    done tools section fully

--- src/pages/Home/sections/Tool.js
+++ src/pages/Home/sections/Tool.js
@@ -4,13 +4,15 @@ const Tool = ({ tool }) => {
     const { name, img, description, min_order_quan, quantity, price } = tool;
     return (
         <div className='rounded-2xl bg-base-100 shadow-lg relative'>
-            <figure className='px-10 pt-10 mb-32'>
-                <img src={img} alt="" />
+            <figure className='px-10 pt-10 mb-56'>
+                <img className='mx-auto' src={img} alt="" />
             </figure>
             <div className='card-body items-center text-center absolute bottom-1'>
                 <h2 className='card-title'>{name}</h2>
                 <p><strong>Minimum Order Quantity:</strong>{min_order_quan}</p>
+                <p><strong>Total Quantity:</strong>{quantity}</p>
                 <p>{description}</p>
+                <p><strong>Price per Piece:</strong> {price}</p>
             </div>
         </div>
     );
--- src/pages/Home/sections/Tools.js
+++ src/pages/Home/sections/Tools.js
@@ -10,8 +10,8 @@ const Tools = () => {
     }, [])
     return (
         <div>
-            <h2 className='text-center'>Manufactured Tools</h2>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 p-40'>
+            <h2 className='text-center mt-12 mb-6 text-4xl text-primary font-Mont font-bold'>Manufactured Tools</h2>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 px-40'>
                 {
                     tools?.map(tool => <Tool key={tool._id} tool={tool}></Tool>)
                 }
