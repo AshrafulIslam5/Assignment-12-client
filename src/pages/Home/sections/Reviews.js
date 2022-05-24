83fe2185c34193de20f224d0b9836d886b926b65
AshrafulIslam5 <ashraful.islam.mihad@gmail.com>
2022-05-24 06:17:00 +0600

    filtering reviews to go away after adding one and only keeping 3

--- src/pages/Home/sections/Reviews.js
+++ src/pages/Home/sections/Reviews.js
@@ -8,12 +8,16 @@ const Reviews = () => {
             .then(res => res.json())
             .then(data => setReviews(data))
     }, [])
+
+    // console.log(reviews?.slice(reviews[reviews.length - 1], 2));
+
+    const lastReview = reviews[reviews?.length - 1]
     return (
         <div>
             <h2 className='text-center my-20 text-4xl'>Our <span className='font-Mont font-extrabold text-primary'>C</span>ustomer's Reviews</h2>
             <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-3 md:px-20 mb-30">
                 {
-                    reviews.map(review => <Review key={review._id} review={review}></Review>)
+                    reviews.slice(Math.max(reviews.length - 5, reviews.length - 3)).map(review => <Review key={review._id} review={review}></Review>)
                 }
             </div>
         </div>
